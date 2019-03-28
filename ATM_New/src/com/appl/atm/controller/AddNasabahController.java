59f58eea4f57d5533051f387430ec05f677f1b61
ImandaSyahrul <ImandaSyahrul@github.com>
2019-03-28 19:11:28 +0700

    Add nasabah(admin)

---
+++ ATM_New/src/com/appl/atm/controller/AddNasabahController.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.appl.atm.controller;
+
+/**
+ *
+ * @author Imanda Syahrul R
+ */
+public class AddNasabahController {
+    
+}
